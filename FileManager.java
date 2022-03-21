ade5bc10df813de0adf47a365163d6a465d44094
Henry Patrick Karugendo <henry.p.karugendo@gmail.com>
2022-03-21 21:50:47 +0000

    Only Commit With Functional Program

---
+++ FileManager.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class FileManager {
+
+	public static void main(String[] args) {
+		final String DIR_STRING = "C:/Users/owner/Downloads";
+		final File MAIN_DIR = new File(DIR_STRING);
+		int moved = 0;
+		int folder = 1;
+		final int limit = 2000;
+		
+		try {
+			for(final File f: MAIN_DIR.listFiles()) {
+				File fd = new File(DIR_STRING + "/" + folder);
+				
+				if(!fd.exists()) {
+					fd.mkdir();
+				}
+				
+				if(!f.isDirectory()) {
+					Files.move(Paths.get(DIR_STRING + "/" + f.getName()), 
+							Paths.get(DIR_STRING + "/" + folder + "/" + f.getName()), 
+							StandardCopyOption.REPLACE_EXISTING);
+					moved++;
+				}
+				
+				if(moved == limit) {
+					moved = 0;
+					folder++;
+					System.out.println("Moved " + limit + " Files!");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR MOVING FILES!!!");
+		}
+		
+		
+	}
+
+}
